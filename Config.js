5e8e8ca6bb55113cf773419cbf0c36256b8abd2e
Saneesh VS <sanufeliz@gmail.com>
2018-03-19 18:52:47 +0530

    supper coif

--- Config.js
+++ Config.js
@@ -1,8 +1,8 @@
 var mysql = require('mysql');
 var pool  = mysql.createPool({
-  host: "mysql7001.site4now.net",
-      user: "a2fe2e_connect",
-      password: "Loveyou123",
+  host: "mysql5011.site4now.net",
+      user: "a16b8c_connect",
+      password: "connected7273",
       database: "db_a2fe2e_connect"
 
     //   host: "localhost",
